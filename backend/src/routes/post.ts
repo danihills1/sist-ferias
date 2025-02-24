1484411be28caf18329c1898c7be7e3801da7cbb
danihills1 <monteirodanielhill@gmail.com>
2025-02-24 10:05:58 -0300

    atualizacao

--- backend/src/routes/post.ts
+++
@@ -1,9 +0,0 @@
-// routes/cadastroRoutes.ts
-import { Router } from 'express';
-import { login } from '../controllers/cadastroController';
-
-const router = Router();
-
-router.post('/login', login); // Rota de login
-
-export default router;
